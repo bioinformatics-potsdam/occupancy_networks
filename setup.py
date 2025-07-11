bb57947695e3284104087c235307b1727cbd5580
Arne Neumann <onet.programming@arne.cl>
2025-07-11 10:38:14 +0200

    fix fatal error: numpy/arrayobject.h: No such file or directory

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ triangle_hash_module = Extension(
     sources=[
         'im2mesh/utils/libmesh/triangle_hash.pyx'
     ],
+    include_dirs=[numpy_include_dir],
     libraries=['m']  # Unix-like specific
 )
 
@@ -30,6 +31,7 @@ mise_module = Extension(
     sources=[
         'im2mesh/utils/libmise/mise.pyx'
     ],
+    include_dirs=[numpy_include_dir]
 )
 
 # simplify (efficient mesh simplification)
@@ -37,7 +39,8 @@ simplify_mesh_module = Extension(
     'im2mesh.utils.libsimplify.simplify_mesh',
     sources=[
         'im2mesh/utils/libsimplify/simplify_mesh.pyx'
-    ]
+    ],
+    include_dirs=[numpy_include_dir]
 )
 
 # voxelization (efficient mesh voxelization)
@@ -46,6 +49,7 @@ voxelize_module = Extension(
     sources=[
         'im2mesh/utils/libvoxelize/voxelize.pyx'
     ],
+    include_dirs=[numpy_include_dir],
     libraries=['m']  # Unix-like specific
 )
 
