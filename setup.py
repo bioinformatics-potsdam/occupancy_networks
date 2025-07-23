b47c8bc1c9f9bda645b589bb3155de21beb18a79
Arne Neumann <github@arne.cl>
2025-07-23 11:59:35 +0200

    update PyMCubes: don't use outdated version shipped with onet

--- setup.py
+++ setup.py
@@ -25,19 +25,6 @@ pykdtree = Extension(
     include_dirs=[numpy_include_dir]
 )
 
-# mcubes (marching cubes algorithm)
-mcubes_module = Extension(
-    'im2mesh.utils.libmcubes.mcubes',
-    sources=[
-        'im2mesh/utils/libmcubes/mcubes.pyx',
-        'im2mesh/utils/libmcubes/pywrapper.cpp',
-        'im2mesh/utils/libmcubes/marchingcubes.cpp'
-    ],
-    language='c++',
-    extra_compile_args=['-std=c++11'],
-    include_dirs=[numpy_include_dir]
-)
-
 # triangle hash (efficient mesh intersection)
 triangle_hash_module = Extension(
     'im2mesh.utils.libmesh.triangle_hash',
@@ -94,7 +81,6 @@ dmc_cuda_module = CUDAExtension(
 
 # Gather all extension modules
 ext_modules = [
-    mcubes_module,
     triangle_hash_module,
     mise_module,
     simplify_mesh_module,
