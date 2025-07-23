d0bead847f78bb8dea8b494054d930d989e2d9cc
Arne Neumann <github@arne.cl>
2025-07-23 14:03:07 +0200

    fix mcubes imports

--- im2mesh/onet/generation.py
+++ im2mesh/onet/generation.py
@@ -173,10 +173,10 @@ class Generator3D(object):
         t0 = time.time()
         occ_hat_padded = np.pad(
             occ_hat, 1, 'constant', constant_values=-1e6)
-        vertices, triangles = libmcubes.marching_cubes(
+        vertices, triangles = mcubes.marching_cubes(
             occ_hat_padded, threshold)
         stats_dict['time (marching cubes)'] = time.time() - t0
-        # Strange behaviour in libmcubes: vertices are shifted by 0.5
+        # Strange behaviour in mcubes: vertices are shifted by 0.5
         vertices -= 0.5
         # Undo padding
         vertices -= 1
